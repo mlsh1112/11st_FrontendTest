cbbcbfc7a53e0016f043fc93c5425616f84fb7b9
LeeSeohyun <mlsh1112@ajou.ac.kr>
2021-08-21 16:50:25 +0900

    Add Icon position change function

--- src/Home.js
+++ src/Home.js
@@ -1,38 +1,61 @@
 const ICONARR_KEY = 'icon-array'
 const HTML = {
-    "alarm" : "<div class = 'link-box alarm' draggable='true'><span class = 'link' route='/alarm'>알람</span></div>",
-    "memo" : "<div class = 'link-box memo' draggable='true'  ><span class = 'link' route='/memo'>메모</span></div>",
-    "image" : " <div class = 'link-box image' draggable='true' ><span class = 'link' route='/image'>사진</span></div>",
+    "alarm" : "<span class = 'link' id='alarm'  route='/alarm'>alarm</span>",
+    "memo" : " <span class = 'link' id='memo'   route='/memo'>memo</span>",
+    "image" : "<span class = 'link' id='image'  route='/image'>image</span>",
 }
 
 const Icons = document.querySelectorAll(".drop-zone")
+const linkBoxes = document.querySelectorAll('.link-box')
+
 let iconsArray = localStorage.getItem(ICONARR_KEY)
 let inital_iconArr = ['alarm','image','memo']
 
 if(!iconsArray){
-    localStorage.setItem(ICONARR_KEY,JSON.stringify(inital_iconArr))
+    iconsArray=inital_iconArr
 }
 else{
     iconsArray=JSON.parse(iconsArray)
 }
 
-iconsArray.map((el,i)=>{
-    Icons[i].innerHTML = HTML[el]
-})
+renderingIcons()
+linkBoxes[0].addEventListener('dragstart',drag)
+linkBoxes[1].addEventListener('dragstart',drag)
+linkBoxes[2].addEventListener('dragstart',drag)
+Icons[0].addEventListener('dragover',allowDrop)
+Icons[1].addEventListener('dragover',allowDrop)
+Icons[2].addEventListener('dragover',allowDrop)
+Icons[0].addEventListener('drop',drop)
+Icons[1].addEventListener('drop',drop)
+Icons[2].addEventListener('drop',drop)
 
-
-
-
-const alarmIcon = document.querySelector('.alarm')
-const imageIcon = document.querySelector('.image')
-const memoIcon = document.querySelector('.memo')
-
-alarmIcon.addEventListener('dragover',allowDrop)
-imageIcon.addEventListener('dragover',allowDrop)
-memoIcon.addEventListener('dragover',allowDrop)
+let startInx = -1 , startIcon = ''
+let destInx = -1 , destIcon = ''
 
 function allowDrop(ev){
-    console.log(ev)
     ev.preventDefault()
 }
 
+function drag(ev){
+    startIcon = ev.target.innerText
+    startInx=iconsArray.indexOf(startIcon)
+}
+
+function drop(ev){
+    ev.preventDefault()
+    destIcon = ev.target.innerText
+    destInx = iconsArray.indexOf(destIcon)
+
+    iconsArray.splice(startInx,1)
+    iconsArray.splice(destInx,0,startIcon)
+    
+    renderingIcons()
+    window.location.reload()
+}
+
+function renderingIcons(){
+    iconsArray.map((el,i)=>{
+        linkBoxes[i].innerHTML = HTML[el]
+    })
+    localStorage.setItem(ICONARR_KEY,JSON.stringify(iconsArray))
+}
